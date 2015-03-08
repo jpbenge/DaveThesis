86c3963ea0f3ee7fd9a5f148e32752eeb80f4499
jpbenge <jpbenge@gmail.com>
2015-03-08 16:16:23 -0400

    new weapon script

--- Assets/Scripts/JPeezy/PlayerWeaponManager.cs
+++ Assets/Scripts/JPeezy/PlayerWeaponManager.cs
@@ -4,7 +4,12 @@ using System.Collections;
 public class PlayerWeaponManager : MonoBehaviour {
 	
 	public GameObject[] weapons;
+	public AudioClip[] weaponSounds;
+	public float[] weaponShootForce;
+	public float weaponVolume = 1;
 	public int curWeapon;
+	public Transform weaponSpawnPoint;
+	public Transform wileySpawnPoint;
 	
 	// Use this for initialization
 	void Start () {
@@ -24,6 +29,29 @@ public class PlayerWeaponManager : MonoBehaviour {
 						curWeapon = showRadialMenu.getSelection()-1;
 						break;
 			}
+		}
+
+
+		if (Input.GetButtonDown("Fire1"))
+		{
+			FireWeapon(curWeapon);
+		}
+	}
+	void FireWeapon(int w)
+	{
+		GameObject obj;
+		if (w == 12)
+		{
+			obj = (GameObject)Instantiate(weapons[w], wileySpawnPoint.position, Quaternion.identity);
+		}
+		else
+		{
+			obj = (GameObject)Instantiate(weapons[w], weaponSpawnPoint.position, Quaternion.identity);
+		}
+		obj.rigidbody.AddForce(transform.forward * weaponShootForce[w]);
+		if (weaponSounds[w] != null)
+		{
+			AudioSource.PlayClipAtPoint(weaponSounds[w], transform.position, weaponVolume);
 		}
 	}
 }
